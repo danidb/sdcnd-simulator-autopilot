d9ee43b844b28621aea93eca124419cbbde0672e
Daniel Del Balso <daniel.delbalso@gmail.com>
2017-03-01 21:28:54 +0100

    Update model_definition docs, and clean out old testing models.

--- model_definition.py
+++ model_definition.py
@@ -1,11 +1,8 @@
-# The model applied in the exercise is defined herein.
-#
-# It is based on InceptionV3, a sufficiently efficient network for
-# the hardware availalbe to me at the time of writing. The InceptionV3
-# network, with pre-trained weights, is available from Keras.
-#
+# This module encapsulates the model architecture, and
+# the simple operations required to compile the model.
+# Keras is a magnificently simple library for rapid prototyping
+# of neural networks.
 
-from keras.applications.vgg16 import VGG16
 from keras.models import Model, Sequential
 
 from keras.layers import Input, Dense, Flatten, Convolution2D, Dropout
@@ -17,7 +14,10 @@ def steering_model(input_shape, optimizer='adam', loss='mean_squared_error'):
     """ Define InceptionV3-based transfer learning model for steering angle prediction.
 
     Args:
-    input_shape: Shape of the image being input to the model. A tuple of length 2.
+    input_shape: Shape of the image being input to the model. A tuple of length 3.
+    optimizer: The optimizer used to compile the Keras model. The default is an Adam optimizer.
+    loss: Loss function. For this regression task, the default of 'mean_squared_error' is quite
+    suitable.
 
     Returns:
     Compiled keras model for image regression to determine steering angle.
@@ -31,35 +31,21 @@ def steering_model(input_shape, optimizer='adam', loss='mean_squared_error'):
     return mod
 
 
+def nvidia_model(input_shape=(48, 48, 3)):
+    """ A convolutional neural network basd on Bojarski et. al. 2016
 
-def inception_model(input_shape=(48, 48, 3)):
-    """ Define an inceptionV3-based architecture for image regression.
-
-    The purpose of this function is solely to encapsulate the model architecture.
+    This simple efficient network can be rapidly trained for steering angle prediction.
+    The architecture below is heavily based on the neural network in 'End to End Learning for Self Driving Cars'
+    (Bojarski et. al. 2016). The model applied here includes additional subsampling, and further regularization/
+    the inclusion of extra dropout layers to prevent overfitting, and 'memorization' of the training
+    track.
 
     Args:
-    input_shape: Tuple of length 3, the shape of the model input. Must have width, height
-    greater than 48, and 3 color channels.
+    input_shape: The shape of the input tensor, a tuple of length 3. (width, height, n_channels)
 
     Returns:
-    Keras architecture for regression, based on InceptionV3.
+    Keras model for image regression.
     """
-
-    mod = Sequential()
-    mod.add(VGG16(weights='imagenet', include_top=False, input_shape=input_shape))
-    mod.layers.pop()
-
-    # For include_top = False, no classification block is included, it is replced here
-    # to give us back some nice regression.
-    mod.add(Flatten())
-    mod.add(Dense(128, W_regularizer=l2(0.01)))
-    mod.add(Dense(32, W_regularizer=l2(0.01)))
-    mod.add(Dense(16, W_regularizer=l2(0.01)))
-    mod.add(Dense(1))
-
-    return mod
-
-def nvidia_model(input_shape=(48, 48, 3)):
 
     mod = Sequential()
     mod.add(Convolution2D(3, 5, 5, input_shape = input_shape, activation='relu', border_mode='valid'))
